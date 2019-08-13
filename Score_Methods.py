fcb0b1c00f7c272c969f14e4fbe66ec09a8602ec
Kcarpio135 <kevincarpio@gmail.com>
2019-08-13 12:29:07 -0700

    leaderdbord time

--- Score_Methods.py
+++ Score_Methods.py
@@ -4,4 +4,4 @@ from User_Models import *
 
 def OrderedScores():
     lowesttime_users = User.query().order(User.lowest_time).fetch(limit=8)
-    return[(user.user_name, user.lowest_time)for user in lowesttime_users]
+    return[(user.user_name, user.lowest_time/60, user.lowest_time%60)for user in lowesttime_users]
