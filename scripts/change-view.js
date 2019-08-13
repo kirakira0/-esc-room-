3f2961ade7ec91593b1af58b406cc408df0ea32d
Kcarpio135 <kevincarpio@gmail.com>
2019-08-13 16:32:54 -0700

    fixed bookshelf and view5

--- scripts/change-view.js
+++ scripts/change-view.js
@@ -1,4 +1,4 @@
-document.getElementById("view1").style.display = "inline-block"; 
+document.getElementById("view1").style.display = "inline-block";
 document.getElementById("view2").style.display = "none";
 document.getElementById("view3").style.display = "none";
 document.getElementById("view4").style.display = "none";
@@ -12,13 +12,13 @@ const passwordBox = document.querySelector("#password-box");
 //hide password box at beginning
 document.getElementById("password-box").style.display = "none";
 
-let currentView = 1; 
+let currentView = 1;
 
 
 document.onkeydown = checkKey;
 
 function checkKey(e) {
-    
+
     //if (!dialogueRunning) {
 
         e = e || window.event;
@@ -30,28 +30,28 @@ function checkKey(e) {
             }
         }
         else if (e.keyCode == '40') { //DOWN ARROW KEY OR 'S'
-            console.log("down arrow pressed"); 
+            console.log("down arrow pressed");
 
         }
         else if (e.keyCode == '37') { //LEFT ARROW KEY OR 'A'
-            console.log(currentView); 
-                
+
             currentView--;
             if (currentView == 0) {
-                currentView = 8; 
+                currentView = 8;
             }
 
-            showView(); 
+            console.log(currentView);
+            showView();
 
         }
-        else if (e.keyCode == '39') { //RIGHT ARROW KEY OR 'D' 
-            console.log(currentView); 
-            
+        else if (e.keyCode == '39') { //RIGHT ARROW KEY OR 'D'
+
             currentView++;
             if (currentView == 9) {
-                currentView = 1; 
+                currentView = 1;
             }
 
+            console.log(currentView);
             showView(); 
 
         }
@@ -62,13 +62,13 @@ function checkKey(e) {
 function showView() {
     for (let i = 1; i <= 8; i++) {
 
-        const element = document.querySelector(`#view${i}`); 
+        const element = document.querySelector(`#view${i}`);
         if (i === currentView) {
-            element.style.display = "inline-block"; 
+            element.style.display = "inline-block";
         }
         else {
-            console.log(`#view${i}`); 
-            element.style.display = "none"; 
+            console.log(`#view${i}`);
+            element.style.display = "none";
         }
     }
 }
