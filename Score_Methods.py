08923036fe93021b8d2982fe9c16d978ef3e3867
Kcarpio135 <kevincarpio@gmail.com>
2019-08-14 10:10:48 -0700

    Top players implemented into Leaderboard

--- Score_Methods.py
+++ Score_Methods.py
@@ -3,5 +3,5 @@ from google.appengine.ext import ndb
 from User_Models import *
 
 def OrderedScores():
-    lowesttime_users = User.query().order(User.lowest_time).fetch(limit=8)
+    lowesttime_users = User.query().order(-User.lowest_time).fetch(limit=8)
     return[(user.user_name, ((600 - user.lowest_time)/60), ((600 - user.lowest_time)%60))for user in lowesttime_users]
