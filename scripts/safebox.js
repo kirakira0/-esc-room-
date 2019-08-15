d02309f9628059d6058c9d4f9e35817b55bd21f0
kirakira0 <kiratoal0@gmail.com>
2019-08-15 12:24:36 -0700

    updated safe

--- scripts/safebox.js
+++ scripts/safebox.js
@@ -1,5 +1,11 @@
 const safeButton = document.querySelector('#safe-button'); 
+document.getElementById('safe-code').style.display = "block"; 
+correctPassword = "lucille industries"; 
 
 safeButton.addEventListener('click', e=> {
     console.log(document.getElementById('safe-submit').value); 
+    if (document.getElementById('safe-submit').value.toLowerCase().replace(/the /g, '').replace(/[^a-z0-9]/g, "") === correctPassword.toLowerCase().replace(/the /g, '').replace(/[^a-z0-9]/g, "")) {
+        document.getElementById('safe-code').style.display = "none"; 
+    }
+    
 })
