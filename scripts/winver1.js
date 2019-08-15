b5001726414cca0427e909d4295580f40e4073dc
kirakira0 <kiratoal0@gmail.com>
2019-08-15 11:32:24 -0700

    added dialogue to both endings

--- scripts/winver1.js
+++ scripts/winver1.js
@@ -15,28 +15,9 @@ var ending1 = [
     "::ENDING 1: SUCCESSFUL UPLOAD"
 ]
 
-var ending2 = [
-    "::After descending the hidden staircase, you notice an emegency exit.",
-    "::It seems to be unlocked as a result of your manual security override.",
-    "::There is little time left before the laboratory self-destructs,",
-    "::so you opt to leave through the emergency exit.",
-    "::You take the helpful android with you as you leave, saving it from certain detruction.", 
-    "::You're not sure what the future holds for you, whether you ever be able to Upload or whether you will forever remain on Earth,",
-    "::but CC193 seems confident that you'll be ok.",
-    "::And somehow,",
-    "::that's enough.",
-    "::ENDING 2: EMERGENCY EXIT"
-]
-
 index = 0;
 
-if (window.location.href.indexOf('/win-ver2') > -1) { 
-    conversation = ending2; 
-    colonPos = conversation[index].indexOf("::"); 
-    document.getElementById("final-text").textContent = conversation[index].substring(colonPos + 2);
-    document.getElementById("name-text").textContent = conversation[index].substring(0, colonPos);
-}
-else if (window.location.href.indexOf('/win-ver1') > -1) {
+if (window.location.href.indexOf('/win-ver1') > -1) {
     console.log('in game page 1'); 
     conversation = ending1; 
     colonPos = conversation[index].indexOf("::"); 
@@ -54,15 +35,7 @@ winSubmitButton.addEventListener('click', e=> {
     index++; 
 })
 
-// const winSubmitButton2 = document.querySelector('#win-submit-button2'); 
 
-// winSubmitButton2.addEventListener('click', e=> {
-//     colonPos = conversation[index].indexOf("::"); 
-//     document.getElementById("final-text").textContent = conversation[index].substring(colonPos + 2);
-//     document.getElementById("name-text").textContent = conversation[index].substring(0, colonPos);
-//     index++; 
-
-// })
 
 
 
